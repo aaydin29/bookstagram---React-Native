bd94d0c5f94891500a938060aa86a5fcb7d046a2
[epserradresi] <epserradresi@gmail.com>
2023-02-17 19:04:57 +0300

    Social page continues.

--- src/components/cards/PostCard/PostCard.js
+++ src/components/cards/PostCard/PostCard.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 import database from '@react-native-firebase/database';
 import styles from './PostCard.style';
 import parsePostData from '../../../utils/parsePostData';
@@ -28,7 +28,6 @@ const PostCard = () => {
         }
 
         setPosts(posts);
-        console.log(posts);
       });
   }, []);
 
@@ -44,38 +43,45 @@ const PostCard = () => {
   //   }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header_container}>
-        <Image
-          style={styles.profile_image}
-          source={require('../../../assest/images/defaultProfile.png')}
-        />
-        {/* User profile photo */}
-        <View style={styles.name_date}>
-          {userData && userData.profile && userData.profile.name ? (
-            <Text style={styles.user_name}>
-              {userData['-ycRbzr2aq0UepD1JG0jGPGz72Io1'].profile.name}
-            </Text>
-          ) : (
-            <Text style={styles.user_name}>UserName</Text>
-          )}
-          <Text style={styles.date}>date</Text>
-        </View>
-      </View>
-      {/* {posts ? <Text style={styles.message}>{posts.text}</Text> : null} */}
+    <ScrollView>
       {posts.map(post => (
-        <View key={post.text}>
+        <View key={post.text} style={styles.container}>
+          <View style={styles.header_container}>
+            <Image
+              style={styles.profile_image}
+              source={require('../../../assest/images/defaultProfile.png')}
+            />
+            {/* User profile photo */}
+            <View style={styles.name_date}>
+              {userData && userData.profile && userData.profile.name ? (
+                <Text style={styles.user_name}>
+                  {userData['-ycRbzr2aq0UepD1JG0jGPGz72Io1'].profile.name}
+                </Text>
+              ) : (
+                <Text style={styles.user_name}>userName</Text>
+              )}
+              <Text style={styles.date}>date</Text>
+            </View>
+          </View>
+          {/* {posts ? <Text style={styles.message}>{posts.text}</Text> : null} */}
+
           <Text>{post.text}</Text>
+
+          <View style={styles.shared_image_container}>
+            {/* {posts ? (
+              <Image
+                style={styles.shared_image}
+                source={{uri: posts.photo}}
+                //   source={require('../../../assest/images/logo.png')}
+              />
+            ) : null} */}
+            {post.photo && (
+              <Image style={styles.shared_image} source={{uri: post.photo}} />
+            )}
+          </View>
         </View>
       ))}
-      <View style={styles.shared_image_container}>
-        <Image
-          style={styles.shared_image}
-          source={require('../../../assest/images/logo.png')}
-        />
-        {/* User shared photo */}
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
